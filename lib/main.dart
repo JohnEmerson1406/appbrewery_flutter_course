347f09cd83a820835561d074580c45a20698c473
john-emerson14 <john.carv.sousa@gmail.com>
2020-04-15 18:53:20 -0300

    feat: add network image

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
-// void main() => runApp(
-//       MaterialApp(
-//         home: Center(
-//           child: Text('Hello World'),
-//         ),
-//       ),
-//     );
-
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+
+          // material colors link: https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
