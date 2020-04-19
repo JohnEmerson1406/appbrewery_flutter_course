2af0e66767d923b387b6cf9fcd75db7691060b62
john-emerson14 <john.carv.sousa@gmail.com>
2020-04-19 16:51:50 -0300

    feat: create question class

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,10 +29,12 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   int questionNumber = 0;
@@ -48,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,6 +75,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctAnswer == true) {
+                  print('dale');
+                } else {
+                  print('uekkk');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
@@ -92,13 +103,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+
+                if (correctAnswer == false) {
+                  print('dale');
+                } else {
+                  print('uekkk');
+                }
+
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.close,
-                      color: Colors.red,
-                    ),
-                  );
+                  questionNumber++;
                 });
               },
             ),
